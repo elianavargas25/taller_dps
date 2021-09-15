7ec8cfb478ef32a0fee2af4883c767013b6e76de
eliana.vargas <eliana.vargas@quipux.com>
2021-09-14 22:14:14 -0500

    Se agregan cambios

--- service-bookings/src/main/java/co/com/poli/bookings/services/BookingServiceImpl.java
+++ service-bookings/src/main/java/co/com/poli/bookings/services/BookingServiceImpl.java
@@ -1,8 +1,15 @@
 package co.com.poli.bookings.services;
 
+import co.com.poli.bookings.client.MovieClient;
+import co.com.poli.bookings.client.ShowtimeClient;
+import co.com.poli.bookings.client.UserClient;
 import co.com.poli.bookings.entities.Booking;
+import co.com.poli.bookings.model.Movie;
+import co.com.poli.bookings.model.Showtime;
+import co.com.poli.bookings.model.User;
 import co.com.poli.bookings.repositories.BookingRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +17,12 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class BookingServiceImpl implements  BookingService{
+public class BookingServiceImpl implements BookingService {
 
     private final BookingRepository bookingRepository;
+    private final UserClient userClient;
+    private final ShowtimeClient showtimeClient;
+    private final MovieClient movieClient;
 
     @Override
     @Transactional(rollbackFor = Exception.class)
@@ -41,6 +51,23 @@ public class BookingServiceImpl implements  BookingService{
     @Override
     @Transactional(readOnly = true)
     public Booking findByUserId(Long userId) {
+        Booking booking = bookingRepository.findByUserId(userId);
+        ModelMapper modelMapper = new ModelMapper();
+        User user =
+                modelMapper.map(userClient.findById(booking.getUserId()).getData(),
+                        User.class);
+        booking.setUser(user);
+
+        Showtime showtime =
+                modelMapper.map(showtimeClient.findById(booking.getShowtimeid()).getData(),
+                        Showtime.class);
+        booking.setShowtime(showtime);
+
+        Movie movie =
+                modelMapper.map(movieClient.findById(booking.getMoviesid()).getData(),
+                        Movie.class);
+        booking.setMovie(movie);
+
         return bookingRepository.findByUserId(userId);
     }
 
